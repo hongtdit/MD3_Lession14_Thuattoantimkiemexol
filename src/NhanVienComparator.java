5ef293eb7b429c59c52263984b473b43f1d7899e
hongtdit <trandinhhong1997@gmail.com>
2023-03-24 13:53:31 +0700

    Initial commit

---
+++ src/NhanVienComparator.java
@@ -0,0 +1,8 @@
+import java.util.Comparator;
+
+public class NhanVienComparator implements Comparator<NhanVien> {
+    @Override
+    public int compare(NhanVien o1, NhanVien o2) {
+        return (int) (o1.getSalary()-o2.getSalary());
+    }
+}
